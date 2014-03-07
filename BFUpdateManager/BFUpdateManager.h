b7a183213a0a69fda060871b9a04d11aa1393e2d
Balazs Faludi <balazsfaludi@gmail.com>
2014-03-07 16:53:08 +0100

    Initial Commit

---
+++ BFUpdateManager/BFUpdateManager.h
@@ -0,0 +1,13 @@
+//
+//  BFUpdateManager.h
+//  BFUpdateManager
+//
+//  Created by Balázs Faludi on 07.03.14.
+//  Copyright (c) 2014 Balazs Faludi. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface BFUpdateManager : NSObject
+
+@end
---
+++ BFUpdateManager/BFUpdateManager.m
@@ -0,0 +1,13 @@
+//
+//  BFUpdateManager.m
+//  BFUpdateManager
+//
+//  Created by Balázs Faludi on 07.03.14.
+//  Copyright (c) 2014 Balazs Faludi. All rights reserved.
+//
+
+#import "BFUpdateManager.h"
+
+@implementation BFUpdateManager
+
+@end
